4bd80cc7fa05b6c37b27382ee0302d5b28e2380c
abhix079 <abhishek788k@gmail.com>
2025-02-06 00:17:41 +0530

    bit

---
+++ Bit Manipulation/bitwiseAnd.java
@@ -0,0 +1,10 @@
+public class bitwiseAnd{
+    public static void main(String args[]){
+        System.out.println(5&6);
+    }
+
+}
+//   0 & 0 =0
+//    0 &1 =0
+//    1 & 0 = 0
+//     1&1= 1
---
+++ Bit Manipulation/bitwiseOr.java
@@ -0,0 +1,13 @@
+
+
+public class bitwiseOr {
+    public static void main(String args[]){
+        System.out.println(5 | 6);
+    }
+    
+}
+
+//   0 | 0 =0
+//   0 | 1 = 1
+//   1 | 0 = 1
+//   1 | 1 = 1
