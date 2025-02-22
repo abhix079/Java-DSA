6390d54eec387de112d6dee1dd30af889ae4205f
abhix079 <abhishek788k@gmail.com>
2025-02-22 13:08:24 +0530

    String

--- Strings/stringBuilder.java
+++ Strings/stringBuilder.java
@@ -1,10 +1,23 @@
 class stringBuilder{
     public static void main(String args[]){
-        StringBuilder sb = new StringBuilder(""); // string builder is initialized empty
+        String str= "Abhishek";
+        StringBuilder sb = new StringBuilder(str); 
 
-        for (char ch ='a';ch<='z';ch++){
-            sb.append(ch);
-        }
+        sb.append("world");
         System.out.println(sb);
+        sb.setCharAt(2,'I');
+        System.out.println(sb);
+        sb.insert(0,"hello"); // used to insert word
+        sb.insert(2,'R');
+        System.out.println(sb);
+        sb.deleteCharAt(2);
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+        sb.delete(0, 2);
+        System.out.println(sb);
+
+
+
     }
 }
