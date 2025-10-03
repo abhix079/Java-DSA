cb034ea2312be2fae5337d0e066ead21b4dff93f
abhix079 <abhishek788k@gmail.com>
2025-10-03 12:14:04 +0530

    greedy algo

---
+++ Greedy/MaxLenChainPair.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MaxLenChainPair {
+    public static void main(String[] args) {
+        
+        int pairs[][] = {{5,24},{39,60},{5,28},{27,40},{50,90}};
+
+        Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
+        int ans=1;//1 to hoga hic
+
+        int chainEnd= pairs[0][1];
+
+        for(int i =1;i<pairs.length;i++){
+            if(pairs[i][0]>chainEnd){
+ans++;
+chainEnd= pairs[i][1];
+            }
+
+        }
+System.out.println(ans);
+
+    }
+    
+}
