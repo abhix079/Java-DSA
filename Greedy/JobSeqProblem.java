dd5cf966170ca3a5b3f04a253c168b7ba26915d2
abhix079 <abhishek788k@gmail.com>
2025-10-03 13:49:36 +0530

    greedy algo

--- Greedy/JobSeqProblem.java
+++ Greedy/JobSeqProblem.java
@@ -35,9 +35,10 @@ public class JobSeqProblem {
             }
         }
 //print seq
-System.out.println("Maximum job : " + seq.size());
+
+System.out.println("Maximum jobs: "+seq.size());
 for(int i =0;i<seq.size();i++){
-System.out.print("Job"+seq.get(i)+" ");
+    System.out.print("Job"+seq.get(i)+" ");
 }
 
 
